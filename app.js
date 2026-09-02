865e6ed6c07e34f3f58f30e8f9628c6bd38d42ef
gulfiya-stack <atasheva.gul1995@gmail.com>
2026-09-02 21:33:38 +0900

    fix: app.js

--- app.js
+++ app.js
@@ -3,35 +3,51 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 
-
 // MongoDB chaqirish
-const db = require("./server").db("Reja");
-
+const db = require("./server").db();
 
 // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // 2: Session code
-
-
 
 // 3: Views code
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-
 // 4: Routing code
 app.post("/create-item", (req, res) => {
-    // TODO: code with db here
-});
+    console.log(req.body);
 
+    const new_reja = req.body.reja;
+
+    db.collection("plans").insertOne(
+        { reja: new_reja },
+        (err, data) => {
+            if (err) {
+                console.log(err);
+                res.end("something went wrong");
+            } else {
+                res.end("successfully added");
+            }
+        }
+    );
+});
 
 app.get("/", function (req, res) {
-    res.render("reja");
+    db.collection("plans")
+        .find()
+        .toArray((err, data) => {
+            if (err) {
+                console.log(err);
+                res.end("something went wrong");
+            } else {
+                console.log(data);
+                res.render("reja", { items: data });
+            }
+        });
 });
-
 
 module.exports = app;
