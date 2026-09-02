71d6acdeeaec1bf50c5eabb8dfa5605e7dce4bc7
gulfiya-stack <atasheva.gul1995@gmail.com>
2026-09-02 21:14:03 +0900

    feat: Task B

--- server.js
+++ server.js
@@ -1,54 +1,38 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-
-console.log('Web serverni boshlash');
-const express = require ("express");
 const http = require("http");
-const app = express();
-const fs = require("fs");
+const mongodb = require("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data);
+let db;
+
+const connectionString =
+    "mongodb+srv://guli-stack:YOUR_PASSWORD@cluster0.lkjwjb0.mongodb.net/Reja?authSource=admin";
+
+mongodb.MongoClient.connect(
+    connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err, client) => {
+        if (err) {
+            console.log("ERROR on connection MongoDB", err);
+        } else {
+            console.log("MongoDB connection succeed");
+
+            module.exports = client;
+
+            const app = require("./app");
+            const server = http.createServer(app);
+
+            let PORT = 3000;
+
+            server.listen(PORT, function () {
+                console.log(
+                    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+                );
+            });
+        }
     }
-});
-// 1 - Kirish code
-app.use(express.static('public')); //CSS, images joylash
-app.use(express.json()); //Kirib kelayotgan client va web-server ortasidagi json format data objectga o'giradi
-app.use(express.urlencoded({extended:true})); // HTML form requestdan expressga qabul qilish
-
-//2: Session code
-
-//3: Views code
-// BSSR - Backend Side Server Rendering - Backend-->Frontend yaratish
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-//4: Routing code
-app.post("/create-item", (req, res) => {
-    // TODO: Code with DB here
-    // console.log(req.body);
-    // res.json({test: "success"});
-});
-app.get("/", function(req, res) {
-    res.render("reja");
-});
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-});
-// app.get("/", function (req, res){
-//     res.end(`<h1 style="background:pink">HELLO WORLD by Guli</h1>`);
-// });
-// app.get("/main", function (req, res){
-//     res.end("<h1>You are on the main page</h1>");
-// });
-// app.get("/gift", function (req, res){
-//     res.end("<h1>You are on gifts page</h1>");
-//});
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function (){
-    console.log(`The server is running successfully on port ${PORT}, http://localhost:${PORT}`);
-});
+);
--- train.js
+++ train.js
@@ -1,34 +1,51 @@
                                                  // TASK-A
-const prompt = require("prompt-sync")();
-function countLetters() {
-    let char = prompt("Harf kiriting: ").toLowerCase();
-    function checkChar () {
-        char.length;
-        while (char.length !==1) {
-            console.log("Sizga 1 harfdan ko'p kiritdingiz, qaytadan kiriting!");
-            char = prompt("Harf kiriting: ").toLowerCase();
-        } 
+// const prompt = require("prompt-sync")();
+// function countLetters() {
+//     let char = prompt("Harf kiriting: ").toLowerCase();
+//     function checkChar () {
+//         char.length;
+//         while (char.length !==1) {
+//             console.log("Sizga 1 harfdan ko'p kiritdingiz, qaytadan kiriting!");
+//             char = prompt("Harf kiriting: ").toLowerCase();
+//         } 
         
-    }
-    checkChar();
+//     }
+//     checkChar();
     
-    const input = prompt("So'z kiriting: ").toLowerCase();;
-    console.log(`Harf va so'z: ${char} va ${input}`);
-    const lst = input.split("");
+//     const input = prompt("So'z kiriting: ").toLowerCase();;
+//     console.log(`Harf va so'z: ${char} va ${input}`);
+//     const lst = input.split("");
     
+//     let count = 0;
+//     lst.forEach((item) => {
+        
+//             if (item  === char) {
+//                 count ++;
+//             }
+//         });
+//     if (count===0) {
+//             console.log(`Siz kiritgan harf '${char}' bu '${input}' so'zida yo'q`);
+//         }
+//     else {console.log(`${char} '${input}'so'zida ${count} harf bor`);}
+    
+// }
+    
+    
+// countLetters();
+
+
+// Task B
+function countDigits(str) {
     let count = 0;
-    lst.forEach((item) => {
-        
-            if (item  === char) {
-                count ++;
-            }
-        });
-    if (count===0) {
-            console.log(`Siz kiritgan harf '${char}' bu '${input}' so'zida yo'q`);
+
+    str.split("").forEach((char) => {
+        if (!isNaN(char) && char !== " ") {
+            count++;
         }
-    else {console.log(`${char} '${input}'so'zida ${count} harf bor`);}
-    
+    });
+
+    return count;
 }
-    
-    
-countLetters();
+
+console.log(countDigits("fdijj583vdf9dsd90sd022g0djj4jv9s")); 
+console.log(countDigits("guli-stack-1995")); 
