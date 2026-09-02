bcafb61dd3b3164b318d213bd447e2278cbb5846
gulfiya-stack <atasheva.gul1995@gmail.com>
2026-09-02 19:03:16 +0900

    fix: refactoring

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post("/create-item", (req, res) => {
     // res.json({test: "success"});
 });
 app.get("/", function(req, res) {
-    res.render("harid");
+    res.render("reja");
 });
 app.get("/author", (req, res) => {
     res.render("author", {user: user});
@@ -50,5 +50,5 @@ app.get("/author", (req, res) => {
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function (){
-    console.log(`The server is running successfully on port ${PORT}`);
+    console.log(`The server is running successfully on port ${PORT}, http://localhost:${PORT}`);
 });
